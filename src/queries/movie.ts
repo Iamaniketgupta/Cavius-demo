971f7eb81673b1f4123bb7a8637b812c6769e7e0
Aniket Gupta <aniketgupta.org@gmail.com>
2025-02-17 00:04:19 +0530

    fix

--- src/queries/movie.ts
+++ src/queries/movie.ts
@@ -1,14 +1,14 @@
 import axiosInstance from "../api/axios"
 const TOKEN: string = import.meta.env.VITE_ACCESS_TOKEN.trim();
- 
 
+const headers = {
+    "accept": "application/json",
+    "Authorization": `Bearer ${TOKEN}`
+}
 export const getPopularMovies = async () => {
     try {
-        const res = await axiosInstance.get(`/movie/popular?language=en-US&page=1`,{
-            headers: {
-                'Content-Type': 'application/json',
-                'authorization': `Bearer ${TOKEN}`,  
-              },
+        const res = await axiosInstance.get(`/movie/popular?language=en-US&page=1`, {
+            headers
         })
         // console.log({pop:res.data.results})
         return res.data.results
@@ -19,11 +19,8 @@ export const getPopularMovies = async () => {
 }
 export const getNowPlayingMovies = async () => {
     try {
-        const res = await axiosInstance.get(`/movie/now_playing?language=en-US&page=1`,{
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${TOKEN}`,  
-              },
+        const res = await axiosInstance.get(`/movie/now_playing?language=en-US&page=1`, {
+            headers
         })
         // console.log({lat:res.data.results})
 
@@ -35,11 +32,8 @@ export const getNowPlayingMovies = async () => {
 }
 export const getTopRatedMovies = async () => {
     try {
-        const res = await axiosInstance.get(`/movie/top_rated?language=en-US&page=1`,{
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${TOKEN}`,  
-              },
+        const res = await axiosInstance.get(`/movie/top_rated?language=en-US&page=1`, {
+           headers
         })
         // console.log({top:res.data.results})
 
@@ -51,11 +45,8 @@ export const getTopRatedMovies = async () => {
 }
 export const getUpComingMovies = async () => {
     try {
-        const res = await axiosInstance.get(`/movie/upcoming?language=en-US&page=1`,{
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${TOKEN}`,  
-              },
+        const res = await axiosInstance.get(`/movie/upcoming?language=en-US&page=1`, {
+           headers
         })
         // console.log({up:res.data.results})
 
@@ -69,11 +60,8 @@ export const searchMovies = async (query: string) => {
     if (!query)
         return;
     try {
-        const res = await axiosInstance.get(`/search/movie?query=${query}&include_adult=false&language=en-US&page=1`,{
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${TOKEN}`,  
-              },
+        const res = await axiosInstance.get(`/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, {
+           headers
         })
         // console.log({search:res.data.results})
 
